70a667cff21390e8a4812c36f4af1c26e9d65f24
olhavalchak <o.valchak@do-it.co>
2021-12-06 15:58:15 +0200

    Add timer for birthday.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect,useState } from 'react';
 function App() {
+  const [days, setDays] = useState();
+  const [hours, setHours] = useState();
+  const [minutes, setMinutes] = useState();
+  const [seconds, setSeconds] = useState();
+
+  const birthday = new Date("Dec 08,2022 16:57:00").getTime();
+  const present = new Date().getTime();
+  const [difference,setDifference] = useState(birthday - present);
+
+  const addYear = () => {
+    setDifference(birthday - present);
+    setDays(Math.floor(difference / (1000 * 60 * 60 * 24)));
+    setHours(Math.floor((difference / (1000 * 60 * 60)) % 24));
+    setMinutes(Math.floor((difference / 1000 / 60) % 60));
+    setSeconds(Math.floor((difference / 1000) % 60));
+    console.log(difference);
+  }
+
+  useEffect (() => {
+    const interval = setInterval(addYear, 1000);
+    // console.log(seconds);
+    return () => {
+      clearInterval(interval);
+    }
+  }, [difference])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="timer-container">
+        <div className="title"> 
+          <span className="its">It's</span>
+          <div className="border">
+            <div className ="time-numbers">
+              <span>{days}</span>
+              <span> : </span>
+              <span>{hours}</span>
+              <span> : </span>
+              <span>{minutes}</span>
+              <span> : </span>
+              <span>{seconds}</span>
+            </div>
+            <div className="time-words">
+              <span>days</span>
+              <span> : </span>
+              <span>hours</span>
+              <span> : </span>
+              <span>minutes</span>
+              <span> : </span>
+              <span>seconds</span>
+            </div>
+          </div>
+          <div>untill your next birthday</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +13,51 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// import React, { useEffect,useState } from 'react';
+// function App() {
+//   const [days, setDays] = useState();
+//   const [hours, setHours] = useState();
+//   const [minutes, setMinutes] = useState();
+//   const [seconds, setSeconds] = useState();
+
+//   // const [date, setDate] = useState(new Date().getFullYear());
+//   const birthday = new Date("Dec 03,2021 16:57:00").getTime();
+//   const present = new Date().getTime();
+//   const [difference,setDifference] = useState(birthday - present);
+
+//   // const addYear = () => {
+//   // setDifference(birthday - present);
+//   //   if( difference >= 0)  {
+//   //     setDays(Math.floor(difference / (24 * 60 * 60 * 1000)));
+//   //     setHours(Math.floor(difference / (60 * 60 * 1000)));
+//   //     setMinutes(Math.floor(difference / (60 * 1000)));
+//   //     setSeconds(Math.floor(difference / (1000)));
+//   //   }
+//   // };
+//   useEffect (() => {
+//     const interval = setInterval(
+//       () => {
+//         setDifference(birthday - present);
+//           if( difference >= 0)  {
+//             console.log(difference);
+//             setDays(Math.floor(difference / (24 * 60 * 60 * 1000)));
+//             setHours(Math.floor(difference / (60 * 60 * 1000)));
+//             setMinutes(Math.floor(difference / (60 * 1000)));
+//             setSeconds(Math.floor(difference / (1000)));
+//           }
+//         }, 1000);
+//     console.log(difference);
+//     return clearInterval(interval);
+//   }, [difference, birthday, present])
+
+//   return (
+//     <div className="container">
+//       <div className="timer-container">
+//         <div className="title">until your next birthday</div>
+//         <span>{days}</span> <span>{hours}</span> <span>{minutes}</span> <span>{seconds}</span>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
